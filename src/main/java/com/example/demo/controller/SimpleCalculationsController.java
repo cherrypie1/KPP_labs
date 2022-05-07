cde0830f7d064e42b54254ae61f6ebcea731fae3
Naty <natashaglushskova@gmail.com>
2022-05-07 11:53:10 +0300

    lab6

--- src/main/java/com/example/demo/controller/SimpleCalculationsController.java
+++ src/main/java/com/example/demo/controller/SimpleCalculationsController.java
@@ -7,11 +7,12 @@ import com.example.demo.service.SimpleCalculationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 public class SimpleCalculationsController {
@@ -33,5 +34,29 @@ public class SimpleCalculationsController {
     @GetMapping("/counter")
     public ResponseEntity<Integer> getCount(){
         return new ResponseEntity<>(CounterService.getCounter(), HttpStatus.OK);
+    }
+
+    @PostMapping("/calculate")
+    public ResponseEntity<?> calculateBulkOperation(@Valid @RequestBody List<InputParams> bodyList) {
+        List<Integer> resultList = new LinkedList<>();
+        bodyList.forEach((currentElem) -> {
+            resultList.add(service.calculate(currentElem).getValue());
+        });
+
+        double averageResult = 0;
+        if (!resultList.isEmpty()) {
+            averageResult = resultList.stream().mapToInt(Integer::intValue).average().getAsDouble();
+        }
+        int maxResult = 0;
+        if (!resultList.isEmpty()) {
+            maxResult = resultList.stream().mapToInt(Integer::intValue).max().getAsInt();
+        }
+        int minResult = 0;
+        if (!resultList.isEmpty()) {
+            minResult = resultList.stream().mapToInt(Integer::intValue).min().getAsInt();
+        }
+
+        return new ResponseEntity<>(resultList + "\nAverage: " + averageResult + "\nMax result: " +
+                maxResult + "\nMin result: " + minResult, HttpStatus.OK);
     }
 }
